893161bde07254bab39e5a0f05226d225fca2aa4
Stupid <yx631@uowmail.edu.au>
2019-03-15 23:20:05 +0800

    defined dataloader

--- vae/dataloader.py
+++ vae/dataloader.py
@@ -1,3 +1,22 @@
 # load images from mp4 with openCV2
-
+import os
 import cv2
+
+class dataloader(object):
+	"""docstring for dataloader"""
+	def __init__(self, foldername="videos"):
+		super(dataloader, self).__init__()
+		self.fileLst = []
+		for file in os.listdir(foldername):
+			if file.endswith(".mp4"):
+				self.fileLst.append(os.path.join(foldername, file))
+
+	def nextFrame():
+		for file in self.fileLst:
+			cap = cv2.VideoCapture(file)
+			while cap.isOpened():
+				ret, frame = cap.read()
+				if ret:
+					yield frame
+			cap.release()
+
