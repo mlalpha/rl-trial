c88eccbd7e79b29070a1e5e4b6cc59cc9c956bef
Stupid <yx631@uowmail.edu.au>
2019-03-16 10:49:24 +0800

    added imports

--- vae/vae.py
+++ vae/vae.py
@@ -1,2 +1,25 @@
 # choose model1 or model2
 # train or use
+
+import dataloader
+import model1
+import model2
+import train
+
+class vae_module(object):
+	"""docstring for vae_module"""
+	def __init__(self, m="model1"):
+		super(vae_module, self).__init__()
+		# self.model = model1.VAE(...)
+		
+	def train(dataset_folder="videos", dataset_format="mp4"):
+		pass
+
+	def save(path="vae.pkl"):
+		pass
+
+	def load(path="vae.pkl"):
+		pass
+
+	def encode(data):
+		pass
