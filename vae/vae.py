9ab3f3c89cd9bad68ec76ac59394af56b709802a
Stupid <yx631@uowmail.edu.au>
2019-03-18 22:24:52 +0800

    module to create, train, encode, save, load with VAE model

--- vae/vae.py
+++ vae/vae.py
@@ -1,25 +1,37 @@
 # choose model1 or model2
 # train or use
 
-import dataloader
+from dataloader import dataloader
 import model1
-import model2
 import train
+import torch
 
 class vae_module(object):
 	"""docstring for vae_module"""
-	def __init__(self, m="model1"):
+	def __init__(self, img_trans=None, dataset_folder="videos",
+				dataset_format="mp4", num_latent, state_size,
+				filter_size=[3, 3, 3], channels=[1, 4, 20, 20]):
 		super(vae_module, self).__init__()
-		# self.model = model1.VAE(...)
+		# init trainloader
+		self.trainloader = dataloader(dataset_folder,
+									dataset_format, img_transform)
+		self.model = model1.VAE(num_latent,
+								state_size, filter_size,
+								channels=)
 		
-	def train(dataset_folder="videos", dataset_format="mp4"):
-		pass
+	def train(iters=26, num_latent=8, print_every=5):
+		train.train_model(self.model,
+			self.trainloader, iters,
+			num_latent, print_every)
 
 	def save(path="vae.pkl"):
-		pass
+		torch.save(self.model.state_dict(), path)
 
 	def load(path="vae.pkl"):
-		pass
+		self.model = model1.VAE(num_latent,
+								state_size, filter_size,
+								channels=)
+		self.model.load_state_dict(torch.load(path))
 
 	def encode(data):
-		pass
+		self.model.enc_func(data)
