73a63ee3842a8407b2ffb87ac8e703f477793b93
Stupid <yx631@uowmail.edu.au>
2019-03-19 13:44:24 +0800

    Bug fix in init(a=1, b) <- should be inti(b, a=1)

--- vae/vae.py
+++ vae/vae.py
@@ -8,8 +8,8 @@ import torch
 
 class vae_module(object):
 	"""docstring for vae_module"""
-	def __init__(self, img_trans=None, dataset_folder="videos",
-				dataset_format="mp4", num_latent, state_size,
+	def __init__(self, num_latent, state_size, img_trans=None,
+				dataset_folder="videos", dataset_format="mp4",
 				filter_size=[3, 3, 3], channels=[1, 4, 20, 20]):
 		super(vae_module, self).__init__()
 		# init trainloader
