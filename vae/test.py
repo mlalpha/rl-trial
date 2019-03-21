1305fcc2372ee1c1015201801c864f12a30c4597
Stupid <yx631@uowmail.edu.au>
2019-03-21 22:51:48 +0800

    better definition, used size 12*12 for test

--- vae/test.py
+++ vae/test.py
@@ -3,9 +3,10 @@ from vae import vae_module
 import cv2
 from dataloader import dataloader
 import numpy as np
+import torch
 
 def use_vae():
-	IMAGE_SIZE = 36
+	IMAGE_SIZE = 12
 
 	def resize(img, size):
 		return cv2.resize(img, size, interpolation=cv2.INTER_AREA)
@@ -21,15 +22,18 @@ def use_vae():
 		gray_image = rgb2gray(img)
 		small_image = resize(gray_image, size=(IMAGE_SIZE, IMAGE_SIZE))
 		matrix = small_image / 255.0
-		return np.expand_dims(matrix, axis=0).astype(np.double)
+		# return np.expand_dims(matrix, axis=0).astype(np.double)
+		return np.expand_dims(matrix, axis=0)
 
 	test_image = load_test_image()
 	test_image = img_transform(test_image)
+	test_image = torch.from_numpy(test_image)
 
 	img_size = IMAGE_SIZE**2
+	num_latent = 5
 
-	vae = vae_module(10, img_size, img_transform)
-	vae.train(26, 10, 5)
+	vae = vae_module(num_latent, img_size, img_transform)
+	vae.train(26, num_latent, 5)
 	print(vae.encode(test_image))
 
 	vae.save()
