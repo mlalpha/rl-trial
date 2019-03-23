b3b9b212b0d55c74b4a22c37f9647368353b6989
Stupid <yx631@uowmail.edu.au>
2019-03-23 21:22:20 +0800

    get buffer loader back and find the bug in mis-use of cv2.isOpened()

--- vae/dataloader.py
+++ vae/dataloader.py
@@ -3,7 +3,6 @@
 from torch.utils.data import Dataset
 import os
 import cv2
-import pickle
 
 class dataloader(Dataset):
 	"""docstring for dataloader"""
@@ -17,13 +16,17 @@ class dataloader(Dataset):
 
 		self.filenframe_idx = []
 		self.len = 0
-		fileIdx = 0
 		for filename in self.fileLst:
 			cap = cv2.VideoCapture(filename)
-			if cap.isOpened():
-				self.len += int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-				for frameIdx in range(0, self.len - 1):
+			fileIdx = 0
+			while cap.isOpened():
+				ret, frame = cap.read()
+				if frame is None:
+					break
+				if ret:
+					self.len += 1
 					self.filenframe_idx.append([fileIdx, frameIdx])
+					frameIdx += 1
 			cap.release()
 			fileIdx += 1
 
@@ -44,3 +47,45 @@ class dataloader(Dataset):
 				if self.transform:
 					frame = self.transform(frame)
 				return frame # we ignored label here # return image, label
+
+
+class buffer_dataloader(Dataset):
+	"""docstring for buffer_dataloader"""
+	def __init__(self, folder_name="videos", filename_extension=".mp4", transform=None,
+				buffer_folder_name="buffer"):
+		super(buffer_dataloader, self).__init__()
+		self.transform = transform
+		fileLst = []
+		for file in os.listdir(folder_name):
+			if file.endswith(filename_extension):
+				fileLst.append(os.path.join(folder_name, file))
+
+		if not os.path.exists(buffer_folder_name):
+			os.makedirs(buffer_folder_name)
+		self.fileLst = []
+		filenum = 0
+		for filename in fileLst:
+			filenum += 1
+			cap = cv2.VideoCapture(filename)
+			count = 0
+			while cap.isOpened():
+				ret, frame = cap.read()
+				if frame is None:
+					break
+				if ret:
+					path = os.path.join(buffer_folder_name, str(filenum) + str(count) + ".png")
+					if not os.path.exists(path):
+						cv2.imwrite(path, frame)
+					self.fileLst.append(path)
+					count += 1
+			cap.release()
+
+	def __len__(self):
+		return len(self.fileLst)
+
+	def __getitem__(self, idx):
+		img_name = self.fileLst[idx]
+		image = cv2.imread(img_name)
+		image = self.transform(image)
+
+		return image # we ignored label here # return image, label
