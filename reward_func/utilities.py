32f6530fc451a4de0867d76e696c55e89718c1ee
Stupid <yx631@uowmail.edu.au>
2019-04-25 22:19:21 +0800

    Added reward store and return

--- reward_func/utilities.py
+++ reward_func/utilities.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn.functional as F
+import math
 
-storage = []
+state_storage = []
+reward_storage = []
 
 # The reward in explore stage
 # a = torch.randn(100, 128)
@@ -11,20 +13,71 @@ def cosine_similarity(a, b):
 	return F.cosine_similarity(a, b)
 
 
+def new_episode():
+	global state_storage, reward_storage
+	state_storage.append([])
+	reward_storage.append([])
+
+
 def state_store(s, n=0):
-	global storage
+	global state_storage
 	# save state and get the smallest distance
 	a = state_similarity(s, n)
-	if s not in storage:
-		storage.append(s)
+	# s_not_in_state_storage = True
+	# for episode in state_storage:
+	# 	if s in episode:
+	# 		s_not_in_state_storage = False
+	# if s_not_in_state_storage:
+	# 	state_storage[-1].append(s)
+	state_storage[-1].append(s)
 	return a
+
+
+def reward_store(r):
+	global reward_storage
+	reward_storage.append(r)
+
+
+def _search_none_zero_elements(l):
+	r = []
+	j = 0
+	for i in l:
+		if i:
+			r.append(j, i)
+		j += 1
+	return r
+
+
+def get_episode(e=-1):
+	global state_storage, reward_storage
+	# calc reward_storage[e] to slope/tangent
+	r = _search_none_zero_elements(reward_storage[e])
+	reward = []
+	for i in r:
+		c = math.pi / i[0]
+		if i[1] < 0:
+			f = math.sin
+		else:
+			f = math.cos
+		for j in range(1, i[0]*2):
+			reward[x] += f(c*j) * i[1]
+	reward_slope = []
+	for i in range(len(reward_storage[e])):
+		reward_slope.append(reward[i + 1] - reward[i])
+	return state_storage[e], reward_slope
+
+
+def get_last_reward():
+	global reward_storage
+	return reward_storage[-1][-1]
 
 
 def state_similarity(s, n=5):
 	a = []
-	global storage
-	for x in storage:
-		a.append(cosine_similarity(x, s))
+	global state_storage
+	for episode in state_storage:
+		for x in episode:
+			a.append(cosine_similarity(x, s))
 	a.sort()
 	if n:
 		return a[:n]
@@ -32,8 +85,10 @@ def state_similarity(s, n=5):
 
 
 def init():
-	global storage
-	storage = []
+	global state_storage, reward_storage
+	state_storage = [[]]
+	reward_storage = [[]]
+
 
 def torch_save_model(model):
 	torch.save(model, 'model.pt')
