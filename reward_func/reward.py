ab53ded906c115b40e1e780e8399ccce83f0109c
Stupid <yx631@uowmail.edu.au>
2019-04-28 14:45:05 +0800

    Things all wrong XD

--- reward_func/reward.py
+++ reward_func/reward.py
@@ -15,7 +15,7 @@ hidden = None
 optimizer = None
 largest_reward = 0
 criterion = None
-seq_len = 100
+LR = 0.01
 
 def reward_trans(raw_reward, state):
 	# GRU predict reward train here
@@ -25,9 +25,9 @@ def reward_trans(raw_reward, state):
 			utilities.reward_store(MAX_REWARD)
 		else:
 			utilities.reward_store(-MAX_REWARD)
-		for step, slope in zip(utilities.get_episode()):
-		    # train(state, reward_record)
-			train(step, slope)
+		states, rewards = utilities.get_episode()
+		for step, reward in zip(states, rewards):
+			train_rnn(step, reward)
 		utilities.new_episode()
 		return raw_reward
 
@@ -35,15 +35,18 @@ def reward_trans(raw_reward, state):
 	# store this state and get some cloesest distance (smallest)
 	k = 4000
 	curiosity_reward = np.asarray(utilities.state_store(state, k)).mean() * MAX_REWARD
+	store_reward = raw_reward
 
-	utilities.reward_store(raw_reward)
 	env_reward = 0 # GRU predict reward slope
 	reward = curiosity_reward#reward * 0.9999 + math.abs(reward - env_reward)
 	if not is_train:
-		states = utilities.get_episode()[-100:]
-		if len(states) < seq_len:
-			states.unshift(0)
-		reward += model(states, var(hidden.data)) #suprise_reward
+		# states = utilities.get_episode()[-100:]
+		# if len(states) < seq_len:
+		# 	states.unshift(0)
+		predicted_reward = model(state, var(hidden.data))
+		if suprised: # suprised
+			reward += sth # suprise_reward
+			store_reward = suprise_reward
 
 	if reward > largest_reward:
 		largest_reward = reward
@@ -51,10 +54,16 @@ def reward_trans(raw_reward, state):
 	if largest_reward > MAX_REWARD:
 		largest_reward *= 0.999999
 
+	utilities.reward_store(store_reward)
 	return reward
 
 
-def reward_init(win_state, loss_state, foul_state):
+def reward_replay():
+	pass
+	
+
+def reward_init(state_size, win_state, loss_state, foul_state,
+				rnn_hidden_size=32, rnn_num_layers=2):
 	global model, hidden, optimizer, criterion, WIN, LOSS, FOUL
 
 	WIN = win_state
@@ -63,22 +72,23 @@ def reward_init(win_state, loss_state, foul_state):
 	# release GRU
 	utilities.init()
 
-	lr = 0.01
-	model = RNN(features=features, cls_size=len(chars))
+	model = RNN(state_size, rnn_hidden_size, rnn_num_layers)
 	if cuda:
 	    model.cuda()
-	criterion = nn.CrossEntropyLoss()
-	optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-
-    model.train()
+	criterion = nn.CrossEntropyLoss() #nn.MSELoss()
+	optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9) #Adam(model.parameters(), lr=LR)
     hidden = model.initHidden()
 
 
 def train_rnn(input_data, reward_data):
 	global model, hidden, optimizer, criterion
-	model.zero_grad()
+	# model.zero_grad()
 	output, hidden = model(input_data, var(hidden.data))
 	loss = criterion(output, reward_data)
 	loss.backward()
 	optimizer.step()
+	return output
+
+def predict_rnn(input_data):
+	output, _ = model(input_data, var(hidden.data))
 	return output
