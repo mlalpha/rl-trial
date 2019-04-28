a36a1ed1cfe3220528afcd814b8ecf266cae84bc
Stupid <yx631@uowmail.edu.au>
2019-04-28 13:13:49 +0800

    bug fix for state exam, making suprise reward

--- reward_func/reward.py
+++ reward_func/reward.py
@@ -4,9 +4,9 @@ import utilities
 import numpy as np
 
 MAX_REWARD = 1
-# WIN = 
-# LOSS = 
-# FOUL = 
+WIN = 0
+LOSS = 1
+FOUL = 2
 END_GAME = [WIN, LOSS, FOUL]
 reward = 0
 is_train = True
@@ -15,12 +15,13 @@ hidden = None
 optimizer = None
 largest_reward = 0
 criterion = None
+seq_len = 100
 
 def reward_trans(raw_reward, state):
 	# GRU predict reward train here
 	if state in END_GAME:
 		# if FOUL then end game, can also be FOUL then choose second best action
-		if state is WIN:
+		if state == WIN:
 			utilities.reward_store(MAX_REWARD)
 		else:
 			utilities.reward_store(-MAX_REWARD)
@@ -30,33 +31,35 @@ def reward_trans(raw_reward, state):
 		utilities.new_episode()
 		return raw_reward
 
-	global model, reward, is_train
+	global model, reward
 	# store this state and get some cloesest distance (smallest)
 	k = 4000
 	curiosity_reward = np.asarray(utilities.state_store(state, k)).mean() * MAX_REWARD
 
-	suprise_reward = 0
-	# if model predict slope * MAX_REWARD < 0 and something
-
 	utilities.reward_store(raw_reward)
 	env_reward = 0 # GRU predict reward slope
-	if is_train:
-		reward = reward * 0.9999 + math.abs(reward - env_reward)
-	else:
-		reward = some how suprise_reward
-
-	# merge new & old reward
-	reward *= curiosity_reward
+	reward = curiosity_reward#reward * 0.9999 + math.abs(reward - env_reward)
+	if not is_train:
+		states = utilities.get_episode()[-100:]
+		if len(states) < seq_len:
+			states.unshift(0)
+		reward += model(states, var(hidden.data)) #suprise_reward
 
 	if reward > largest_reward:
 		largest_reward = reward
 	reward /= largest_reward
+	if largest_reward > MAX_REWARD:
+		largest_reward *= 0.999999
 
 	return reward
 
 
-def reward_init():
-	global model, hidden, optimizer, criterion
+def reward_init(win_state, loss_state, foul_state):
+	global model, hidden, optimizer, criterion, WIN, LOSS, FOUL
+
+	WIN = win_state
+	LOSS = loss_state
+	FOUL = foul_state
 	# release GRU
 	utilities.init()
 
